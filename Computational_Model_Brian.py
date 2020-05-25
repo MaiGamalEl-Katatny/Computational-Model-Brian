e2633cd640abab45d47e09683e2bc291df19a929
Mai Gamal <54598724+MaiGamalEl-Katatny@users.noreply.github.com>
2020-05-25 15:41:26 +0200

    Update Computational_Model_Brian.py

--- Computational_Model_Brian.py
+++ Computational_Model_Brian.py
@@ -5,6 +5,8 @@ Created on Sat Jan 25 12:52:13 2020
 @author: maiga
 """
 
+#This project is based on the model presented in: Ramirez-Mahaluf, Juan P., et al. "A computational model of Major Depression: the role of glutamate dysfunction on cingulo-frontal network dynamics." Cerebral cortex 27.1 (2017): 660-679.
+
 import brian2
 import timeit
 from brian2 import *
